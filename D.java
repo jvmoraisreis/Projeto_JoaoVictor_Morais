95d6a677d2c2225c320a4332ff07a561b370b686
jvmoraisreis <jvmoraisreis@outlook.com>
2022-07-04 17:38:20 -0300

    Criação da classe D

---
+++ D.java
@@ -0,0 +1,19 @@
+public class D {
+    //Atributos
+    private char D1;
+    private int D2;
+
+    //Métodos
+    public char getD1() {
+        return D1;
+    }
+    public int getD2() {
+        return D2;
+    }
+    public void MD1() {
+        System.out.println("MD1");
+    }
+    public void MD2() {
+        System.out.println("MD2");
+    }
+}
