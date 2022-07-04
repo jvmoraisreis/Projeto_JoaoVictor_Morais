c38328412531b2205c388dd011b9b2ee6997ba39
jvmoraisreis <58453291+jvmoraisreis@users.noreply.github.com>
2022-07-04 15:58:05 -0300

    Add files via upload

---
+++ A.java
@@ -0,0 +1,27 @@
+public class A {
+    //Atributos
+    private int A1;
+    private float A2;
+
+    //Métodos 
+    public int getA1() {
+        return A1;
+    }
+    public void setA1(int _n) {
+        A1 = _n;
+    }
+    public float getA2() {
+        return A2;
+    }
+    public void setA2(float _n) {
+        A2 = _n;
+    }
+
+    public void MA1() {
+        System.out.println("MA1");
+    }
+    public void MA2() {
+        System.out.println("MA2");
+    }
+}
+
---
+++ B.java
@@ -0,0 +1,26 @@
+public class B {
+    //Atributos
+    private int B1;
+    private float B2;
+
+    //Métodos 
+    public int getB1() {
+        return B1;
+    }
+    public void setB1(int _n) {
+        B1 = _n;
+    }
+    public float getB2() {
+        return B2;
+    }
+    public void setB2(float _n) {
+        B2 = _n;
+    }
+
+    public void MB1() {
+        System.out.println("MB1");
+    }
+    public void MB2() {
+        System.out.println("MB2");
+    }  
+}
